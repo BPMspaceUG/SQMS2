526843a90a8a034e43c49b1360acd19d621b6623
wu <daniel.wurzer@bpmspace.org>
2019-09-09 14:21:15 +0200

    Fixed SQL Typo Order

--- sqldump/dump_sqms2_structure_v1_incl_mindata_diff.sql
+++ sqldump/dump_sqms2_structure_v1_incl_mindata_diff.sql
@@ -1,1 +1,3 @@
- 
+ ALTER TABLE `bpmspace_sqms2_v1`.`sqms2_question_text` 
+CHANGE COLUMN `sqms2_Question_id_fk_783494` `sqms2_Question_id_fk_783494` BIGINT(20) NULL DEFAULT NULL AFTER `sqms2_Question_Text_id`;
+
