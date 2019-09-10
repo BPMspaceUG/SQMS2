3e140adf3677ed9ba8294053e09b2b8844289044
wu <daniel.wurzer@bpmspace.org>
2019-09-10 14:11:03 +0200

    Update dump_sqms2_structure_v1_incl_mindata_diff.sql

--- sqldump/dump_sqms2_structure_v1_incl_mindata_diff.sql
+++ sqldump/dump_sqms2_structure_v1_incl_mindata_diff.sql
@@ -1,3 +1,5 @@
  ALTER TABLE `bpmspace_sqms2_v1`.`sqms2_question_text` 
 CHANGE COLUMN `sqms2_Question_id_fk_783494` `sqms2_Question_id_fk_783494` BIGINT(20) NULL DEFAULT NULL AFTER `sqms2_Question_Text_id`;
 
+ALTER TABLE `bpmspace_sqms2_v1`.`sqms2_question` 
+AUTO_INCREMENT = 85647689 ;
