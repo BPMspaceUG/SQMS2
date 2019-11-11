63c5f552fec210f01dcffe43e6c550332c938e39
Daniel Wurzer <daniel.wurzer@bpmspace.org>
2019-11-11 14:44:03 +0100

    new vers from apms

--- config.EXAMPLE_SECRET.inc.php
+++ config.EXAMPLE_SECRET.inc.php
@@ -1,5 +1,5 @@
 <?php
-    // APMS Generated Project (2019-11-06 15:38:28)
+    // APMS Generated Project (2019-11-11 14:41:34)
     // Version: https://github.com/BPMspaceUG/APMS2/tree/6d5ee2245de2d3b70f49b91fe16e2d896765f425
     // ==================================================
     //-- Database
