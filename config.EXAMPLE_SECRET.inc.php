f6e71e349dcb9ee452a56fcfd3e30ceef285a4b4
Daniel Wurzer <daniel.wurzer@bpmspace.org>
2019-11-20 13:00:37 +0100

    improved statemachine

--- config.EXAMPLE_SECRET.inc.php
+++ config.EXAMPLE_SECRET.inc.php
@@ -1,5 +1,5 @@
 <?php
-    // APMS Generated Project (2019-11-20 11:38:04)
+    // APMS Generated Project (2019-11-20 12:57:43)
     // Version: https://github.com/BPMspaceUG/APMS2/tree/6d5ee2245de2d3b70f49b91fe16e2d896765f425
     // ==================================================
     //-- Database
