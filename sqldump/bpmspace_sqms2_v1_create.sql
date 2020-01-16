eb9e87ab2216a703b8cc4a0fdd8ea19401abcbf3
rootmessages BPMspace <5494549+BPMspace@users.noreply.github.com>
2020-01-16 10:59:08 +0100

    Update bpmspace_sqms2_v1_create.sql

--- sqldump/bpmspace_sqms2_v1_create.sql
+++ sqldump/bpmspace_sqms2_v1_create.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE bpmspace_sqms2_v1;
 CREATE USER 'bpmspace_sqms2_v1'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON bpmspace_sqms2_v1 TO 'bpmspace_sqms2_v1'@'localhost';
+GRANT CREATE, INSERT , SELECT , UPDATE ON bpmspace_sqms2_v1.* TO 'bpmspace_sqms2_v1'@'localhost';
+FLUSH PRIVILEGES;
 USE bpmspace_sqms2_v1;
