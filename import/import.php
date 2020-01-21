a7c495f34c496783066dcc49d27ce34d388b6679
Daniel Wurzer <daniel.wurzer@bpmspace.org>
2020-01-21 14:35:59 +0100

    repaired the esv-wizard

--- import/import.php
+++ import/import.php
@@ -6,17 +6,7 @@
     ini_set('max_execution_time', 300);
     set_time_limit(300);
 
-    // Import works via Path
-    // So for example we can give the following structure:    
-    // cmd = [question/85647895] / [answer/create] / [text/create]
-    /* param = [
-        0: {should be empty...execpt we wanna update something},
-        1: {"correct": true},
-        2: {"text": "this is a correct answer."}
-    ]
-    */
-
     //===> IMPORT
     echo "<pre>";
     $import = new DataImporter(); // data_full data_question
-    $import->importFile(__DIR__."/generated_demo_full.json");
+    $import->importFile(__DIR__."/test_data_question.json");
